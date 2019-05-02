501d1d9a3701517aca48b0f7977a13dc64bd35cb
Tim Untersberger <timuntersberger2@gmail.com>
2019-05-02 11:11:40 +0000

    cleanup build folder and add https

--- FirmwareServer/src/index.ts
+++ FirmwareServer/src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello world")
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+
+const app = express();
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/baaka.io/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/baaka.io/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/baaka.io/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+app.use((req, res) => {
+	res.send('Hello there !');
+});
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(8080, () => {
+	console.log('HTTPS Server running on port 8080');
+});
