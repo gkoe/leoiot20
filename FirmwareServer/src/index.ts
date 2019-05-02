97f5317200d62eb62cbb1b9f819e497d9fa682e4
Tim Untersberger <timuntersberger2@gmail.com>
2019-05-02 11:28:57 +0000

    Add generated build files to gitignore, change port| :

--- FirmwareServer/src/index.ts
+++ FirmwareServer/src/index.ts
@@ -1,27 +1,15 @@
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const express = require('express');
 
 const app = express();
-
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/baaka.io/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/baaka.io/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/baaka.io/chain.pem', 'utf8');
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
 
 app.use((req, res) => {
 	res.send('Hello there !');
 });
 
-const httpsServer = https.createServer(credentials, app);
+const httpServer = http.createServer(app);
 
-httpsServer.listen(8080, () => {
-	console.log('HTTPS Server running on port 8080');
+httpServer.listen(8000, () => {
+	console.log('HTTP Server running on port 8000');
 });
